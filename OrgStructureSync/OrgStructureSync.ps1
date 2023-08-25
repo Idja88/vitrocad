2431774dcd292845dad7ddb8053c1fcb6c59193e
Idja88 <idja08@gmail.com>
2023-08-25 13:16:57 +0600

    Functional Groups and Fixes

--- OrgStructureSync/OrgStructureSync.ps1
+++ OrgStructureSync/OrgStructureSync.ps1
@@ -4,7 +4,8 @@
     [string]$orglistname = "Организационно-штатная структура",
     [string]$fizlistname = "Физические лица",
     [string]$divcontent = "0x0100CE4B4034AA87410EB92561C8318E2C16005639E40A0D3F6342A19FF44EB45B5A85",
-    [string]$empcontent = "0x010040D9D13AF3634AACB514CB30B54F2CAE004BC5116E5FCC734388387FAF0125CB7D"
+    [string]$empcontent = "0x010040D9D13AF3634AACB514CB30B54F2CAE004BC5116E5FCC734388387FAF0125CB7D",
+    [string]$funccontent = "0x0100014407A2350F49DD996BFE060D9B018700F2B87C61FA93FB42AAA238DAB6F082C6"
 )
 
 function Test-ModuleInstalled {
@@ -41,6 +42,7 @@ $orgitems = Get-PnPListItem -List $orglistname
 $fizitems = Get-PnPListItem -List $fizlistname
 $emps = $orgitems | Where-Object {$_.FieldValues.ContentTypeId -like $empcontent -and $_.FieldValues.VitroOrgDisplayInStructure -eq $true}
 $divs = $orgitems | Where-Object {$_.FieldValues.ContentTypeId -like $divcontent -and $_.FieldValues.VitroOrgDisplayInStructure -eq $true} | Select-Object -Skip 1 #Убираем из массива корень структуры
+$funcs = $orgitems | Where-Object {$_.FieldValues.ContentTypeId -like $funccontent -and $_.FieldValues.VitroOrgDisplayInStructure -eq $true}
 
 #Синхронизируем данные учётных записей из AD
 foreach($user in $users){
@@ -74,16 +76,55 @@ foreach ($div in $divs) {
     #Добавляем сотрудника в группу пользователей
     foreach ($divemp in $divemps){
         Add-LoginProperties -Item $divemp -ListItems $fizitems
-
         if($divemp.FieldValues.PersonLogin -notin $grpmbrs.ID){
-            Add-PnPUserToGroup -Identity $divemp.FieldValues.VitroOrgParentId.LookupValue -LoginName $divemp.FieldValues.PersonLogin
+            Add-PnPUserToGroup -Identity $div.FieldValues.GroupId -LoginName $divemp.FieldValues.PersonLogin
         }
     }
 
     #Убираем логин пользователя, который больше не является сотрудником подразделения
     foreach($grpmbr in $grpmbrs){
         if($grpmbr.Id -notin $divemps.FieldValues.PersonLoginId){
-            Remove-PnPUserFromGroup -LoginName $_.LoginName -Identity $div.FieldValues.GroupId
+            Remove-PnPUserFromGroup -LoginName $grpmbr.LoginName -Identity $div.FieldValues.GroupId
+        }
+    }
+}
+
+foreach ($func in $funcs) {
+    #Проверка на существующие группы, как ф.группы и добавление их как параметр
+    $funcGrp = Get-PnPGroup -Identity $func.FieldValues.Title
+    if($null -eq $funcGrp) {
+        $funcGrpNew = New-PnPGroup -Title $func.FieldValues.Title -Owner $admin.UserName
+        $func.FieldValues | Add-Member -MemberType NoteProperty -Name GroupId -Value $funcGrpNew.Id -Force
+        [string]$strFuncGrp = $func.FieldValues.GroupId
+        $FuncGroupValues = @{"VitroOrgLogin" = $strFuncGrp}
+    }
+    else {
+        $func.FieldValues | Add-Member -MemberType NoteProperty -Name GroupId -Value $funcGrp.Id -Force
+        [string]$strFuncGrp = $func.FieldValues.GroupId
+        $FuncGroupValues = @{"VitroOrgLogin" = $strFuncGrp}
+    }
+
+    #Проверка на уже проставленные группы
+    if($func.FieldValues.VitroOrgLogin.LookupId -ne $func.FieldValues.GroupID -or $null -eq $func.FieldValues.VitroOrgLogin.LookupId){
+        Set-PnPListItem -List $orglistname -ContentType "Функциональная группа" -Identity $func.Id -Values $FuncGroupValues
+    }
+
+    #Проверка на соответствие Сотрудников в Группе Пользователей и удаление лишних (перевод сотрудника в др. отдел)
+    $funcemps = $emps | Where-Object {$_.FieldValues.ID -in $func.FieldValues.VitroOrgMemberList.LookupId}
+    $funcgrpmbrs = Get-PnPGroupMembers -Identity $func.FieldValues.GroupId
+
+    #Добавляем сотрудника в группу пользователей
+    foreach ($funcemp in $funcemps){
+        Add-LoginProperties -Item $funcemp -ListItems $fizitems
+        if($funcemp.FieldValues.PersonLogin -notin $funcgrpmbrs.ID){
+            Add-PnPUserToGroup -Identity $func.FieldValues.GroupId -LoginName $funcemp.FieldValues.PersonLogin
+        }
+    }
+
+    #Убираем логин пользователя, который больше не является сотрудником ф.группы
+    foreach($funcgrpmbr in $funcgrpmbrs){
+        if($funcgrpmbr.Id -notin $funcemps.FieldValues.PersonLoginId){
+            Remove-PnPUserFromGroup -LoginName $funcgrpmbr.LoginName -Identity $func.FieldValues.GroupId
         }
     }
 }
