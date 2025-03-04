ea8ff646d76dbae788f691f16839b8b2161bf5c6
Idja88 <idja08@gmail.com>
2025-03-04 11:24:29 +0500

    Monitoring Scripts Added

---
+++ LicenseMonitor/LicenseMonitor.ps1
@@ -0,0 +1,20 @@
+﻿$vitroserver = ""
+
+$counter1 = '\\{0}\Веб-служба(VitroLicenseService)\Количество текущих подключений' -f $vitroserver
+$counter2 = '\\{0}\Веб-служба(VitroLicenseService)\Максимальное количество подключений' -f $vitroserver
+
+$Counters = @(
+    $counter1,
+    $counter2
+)
+$OutputFile = "LicenseCounter.csv"
+
+Get-Counter -Counter $Counters | ForEach {
+    $_.CounterSamples | ForEach {
+        [pscustomobject]@{
+            TimeStamp = $_.TimeStamp
+            Path = $_.Path
+            Value = $_.CookedValue
+        }
+    }
+} | Export-Csv -Path $OutputFile -Append -NoTypeInformation -Encoding UTF8
---
+++ LicenseMonitor/UserMonitor.ps1
@@ -0,0 +1,29 @@
+﻿$vitroserver = ""
+
+$counter1 = '\\{0}\Веб-служба(Sharepoint - 80)\Количество текущих подключений' -f $vitroserver
+$counter2 = '\\{0}\Веб-служба(Sharepoint - 80)\Максимальное количество подключений' -f $vitroserver
+$counter3 = '\\{0}\Веб-служба(Sharepoint - 80)\Количество подключенных анонимных пользователей' -f $vitroserver
+$counter4 = '\\{0}\Веб-служба(Sharepoint - 80)\Максимальное количество анонимных пользователей' -f $vitroserver
+$counter5 = '\\{0}\Веб-служба(Sharepoint - 80)\Количество подключенных неанонимных пользователей' -f $vitroserver
+$counter6 = '\\{0}\Веб-служба(Sharepoint - 80)\Максимальное количество неанонимных пользователей' -f $vitroserver
+
+$Counters = @(
+    $counter1,
+    $counter2,
+    $counter3,
+    $counter4,
+    $counter5,
+    $counter6
+)
+
+$OutputFile = "UserCounter.csv"
+
+Get-Counter -Counter $Counters | ForEach {
+    $_.CounterSamples | ForEach {
+        [pscustomobject]@{
+            TimeStamp = $_.TimeStamp
+            Path = $_.Path
+            Value = $_.CookedValue
+        }
+    }
+} | Export-Csv -Path $OutputFile -Append -NoTypeInformation -Encoding UTF8
